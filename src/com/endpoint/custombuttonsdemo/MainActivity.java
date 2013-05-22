d54a38698cf05f448deca3d5e5dfcf87102599bc
Zed Jensen <zed@endpoint.com>
2013-05-22 08:02:35 +0100

    add button

--- src/com/endpoint/custombuttonsdemo/MainActivity.java
+++ src/com/endpoint/custombuttonsdemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.endpoint.custombuttonsdemo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,8 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void doStuff(View v) {
+		Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
+	}
 }
